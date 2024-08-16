09bc93a65edcf24d2c63683843f1c2f40fd62c69
KauaKley <kley0237@gmail.com>
2024-08-15 23:19:29 -0300

    Update

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -4,32 +4,29 @@ public class Calculadora {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Escolha um desses operadores: +, -, *, /");
-        String operador = scan.nextLine();
+        String operador = scan.next();
         System.out.println("Digite dois numeros: ");
         double numero1 = scan.nextDouble();
         double numero2 = scan.nextDouble();
-        double resultado;
+        double resultado = 0;
 
         switch (operador)
         {
             case "+":
                 resultado = numero1 + numero2;
-                System.out.println(resultado);
                 break;
             case "-":
                 resultado = numero1 - numero2;
-                System.out.println(resultado);
                 break;
             case "*":
                 resultado = numero1 * numero2;
-                System.out.println(resultado);
                 break;
             case "/":
                 resultado = numero1 / numero2;
-                System.out.println(resultado);
                 break;
             default:
                 System.out.println("Operador indisponivel");
         }
+        System.out.println(resultado);
     }
 }
